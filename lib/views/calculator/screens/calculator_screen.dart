bcf9ddc952c7268f0a9fdc1e668e0e16ac85aa6a
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2022-10-11 12:48:24 +0600

    design updated

--- lib/views/calculator/screens/calculator_screen.dart
+++ lib/views/calculator/screens/calculator_screen.dart
@@ -36,42 +36,143 @@ class CalculatorHome extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Container(
-            height: size.height * 0.12,
-            width: size.width * 0.95,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 4,
+          child: Column(
+            children: [
+              Spacer(),
+              Container(
+                height: size.height * 0.12,
+                width: size.width * 0.95,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, top: 15),
-                  child: Text(
-                    'How old am I If I was born on',
-                    style: GoogleFonts.montserrat(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 15),
+                      child: Text(
+                        'How old am I If I was born on',
+                        style: GoogleFonts.montserrat(
+                          color: Color(0xff54585c),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            print('Month Click');
+                          },
+                          child: Text(
+                            'MM/',
+                            style: GoogleFonts.montserrat(
+                              color: Color(0xffcbd4f5),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            print('Date Click');
+                          },
+                          child: Text(
+                            'DD/',
+                            style: GoogleFonts.montserrat(
+                              color: Color(0xffcbd4f5),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            print('Year Click');
+                          },
+                          child: Text(
+                            'YYYY?',
+                            style: GoogleFonts.montserrat(
+                              color: Color(0xffcbd4f5),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: size.height * 0.04),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Your Age in Years : 0',
+                    style: GoogleFonts.raleway(
                       color: Color(0xff54585c),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  'MM/DD/YYYY?',
-                  style: GoogleFonts.montserrat(
-                    color: Color(0xffcbd4f5),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+                  SizedBox(height: 5),
+                  Text(
+                    'Your Age in Months : 0',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xff54585c),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Your Age in Days : 0',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xff54585c),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Your Age in Hours : 0',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xff54585c),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Your Age in Minutes  : 0',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xff54585c),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Your Age in Seconds : 0',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xff54585c),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
           ),
         ),
       ),
